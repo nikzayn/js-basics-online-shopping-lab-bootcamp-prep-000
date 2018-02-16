509633e7ae4196a45c579c2b5aa44b997d08ea21
NIKHIL VAIDYAR <nikhilvaidyar1997@gmail.com>
2018-02-16 16:02:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,20 +18,29 @@ function addToCart(item) {
   return cart;
 }
 
-function viewCart() {
-    if(cart.length===0){
-      console.log("Your shopping cart is empty.");
-    }
-    else{
-      var items = [];
-      for(var i = 0; i < cart.length; i++){
-        for(var item in cart[i]){
-          items.push(item + " at $" + cart[i][item]);
-      }
-     console.log("In your cart, you have " + itemPrice.slice(0, itemPrice.length -1).join(' and ') + ".");
+function viewCart() {if(cart.length === 0){
+  console.log(`Your shopping cart is empty.`)
 }
-}
-}
+else{
+     var myString = "In your cart, you have "
++
++        for(var i = 0, l = cart.length; i < l; i++){
++            for(var list in cart[i]){
++            myString+=`${list} at $${cart[i][list]}`
++                if(i!==cart.length-1){
++                  myString+=", "
++                }
++                else{
++                  myString+="."
++                }
++              }
++
++            }
++
++      console.log(myString)
++  }
++}
++}
 function total()  {
   var sum = 0;
   for (var i=0; i<cart.length; i++) {
